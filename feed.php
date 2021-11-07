d14c12b237b1720fface36460c6ae395c72faf58
stino220802 <79916464+stino220802@users.noreply.github.com>
2021-11-07 23:09:28 +0100

    Create feed.php

---
+++ feed.php
@@ -0,0 +1,19 @@
+
+
+<?xml version='1.0' encoding='UTF-8'?>
+<rss version='2.0'>
+<channel>
+
+<title>iot</title>
+<link>12001732.pxl-ea-ict.be</link>
+<description>iot project</description>
+<language>en-us</language>
+
+<item>
+<title>Item/Article Title</title>
+<link>Item/Article URL</link>
+<description>Items/Article description</description>
+</item>
+
+</channel>
+</rss>
