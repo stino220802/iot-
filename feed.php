b6ea40c30531a4fe57a4dcd7dc86bed63fa0da71
stino220802 <79916464+stino220802@users.noreply.github.com>
2021-11-07 23:28:12 +0100

    Update feed.php

--- feed.php
+++ feed.php
@@ -14,6 +14,40 @@
 <link>Item/Article URL</link>
 <description>Items/Article description</description>
 </item>
+<body> 
+  <h1> RSS feed </h1> 
+  
+  <?php
+include('db.sql');
+$sql = "SELECT * FROM RSS_FEED ORDER BY id "; 
+$query = mysql_query($sql);
+
+header("Content-type: text/xml"); 
+
+echo "<?xml version='1.0' encoding='UTF-8'?>
+<rss version='2.0'>
+<channel>
+<title> RSS feed</title>
+<link> http://12001732.pxl-ea-ict.be/ </link>
+<description>RSS feed iot </description>
+<language>en-us</language>"; 
+
+while($row = mysql_fetch_array($query))
+{
+$title=$row['title']; 
+$link=$row['link']; 
+$description=$row['description']; 
+
+echo "<item>
+<title>$title</title>
+<link>$link</link>
+<description>$description</description>
+</item>"; 
+} 
+echo "</channel></rss>"; 
+?>
+  
+</body>
 
 </channel>
 </rss>
