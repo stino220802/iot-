3f5b701ca05e75be751c12a71917858ff4879dd3
Stijn Verhoeven <12001732@student.pxl.be>
2021-11-28 22:43:03 +0100

    webform
    to submit data to the database from a webform

--- db.sql
+++ db.sql
@@ -1,12 +1,12 @@
 CREATE TABLE future_proof (
  
- sensor_id VARCHAR(250),
+ sensor_id INT,
  value INT,
  created_at timestamp DEFAULT CURRENT_TIMESTAMP
  );
 
 CREATE TABLE lastknown(
- ID VARCHAR(250), 
+ ID INT, 
  name VARCHAR(250), 
  external_ip VARCHAR(250), 
  created_at timestamp DEFAULT CURRENT_TIMESTAMP
