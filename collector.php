13ba2cd002d3fccbe685f9543aa0751892385c95
stino220802 <79916464+stino220802@users.noreply.github.com>
2022-01-14 13:04:59 +0100

    Update collector.php

--- collector.php
+++ collector.php
@@ -36,12 +36,10 @@
 			$sensor = $_POST["id_sensor"];
 			$name = $_POST["naam"];
 			$waarde = $_POST["sensor_value"];
-			if($waarde > 100){
-				$waarde = $waarde / 10;
-			}
+			
 			// get external ip address
 			$ip_addres = $_SERVER['REMOTE_ADDR'];
-						//$ip_addres = "192.00.00"
+						
 
 			//delete previous instance of sensor
 			$sql2 = "DELETE FROM lastknown WHERE ID = '$sensor'";
@@ -50,7 +48,7 @@
 			// put data into future proof table 	
 			$sql3 = "insert into future_proof(sensor_id, value) values('$sensor', '$waarde')";
 			$run = mysqli_query($conn, $sql3) or die(mysqli_error());
-			$ip_addres = "109.130.51.72";
+			
 			// put data into lastknown table
 			$sql4 = "insert into lastknown(ID, name, external_ip) values('$sensor', '$name', '$ip_addres')";
 			$run2 = mysqli_query($conn, $sql4) or die (mysqli_error($conn)); 
